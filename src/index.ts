d08fdf88843c8f75b4352ab781f661a5d3571980
lucasgianine <lucas.esilva@sptech.school>
2025-01-20 12:48:07 -0300

    Cria função para OS Compatibility Test

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import github from '@actions/github'
+import core from '@actions/core'
+
+;(async function coreFunction () {
+    try {
+        const username = core.getInput('username') ?? github.context.actor
+        const os = process.platform
+        const greeting = `Olá, usuário ${username}! Seu sistema operacional é ${os}`
+
+        core.setOutput('greeting', greeting)
+    } catch (err: any) {
+        core.setFailed(`Erro ao executar a Action: ${err.message}`)
+    }
+})()
